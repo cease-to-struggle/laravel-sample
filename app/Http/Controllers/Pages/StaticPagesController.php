f640318bc34d797655ca33c94e63b5f4c488f287
fenglei <chai.fenglei@indzz.cn>
2018-09-06 10:54:13 +0800

    静态页面

---
+++ app/Http/Controllers/Pages/StaticPagesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Pages;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class StaticPagesController extends Controller
+{
+	//TODO:return the home page
+	public function home()
+	{
+		return view('static-pages/home');
+	}
+
+	//TODO:return the help page
+	public function help()
+	{
+		return view('static-pages/help');
+	}
+
+	//TODO:return the discription page
+	public function about()
+	{
+		return view('static-pages/about');
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/','Pages\StaticPagesController@home');
+Route::get('/help','Pages\StaticPagesController@help');
+Route::get('/about','Pages\StaticPagesController@about');
