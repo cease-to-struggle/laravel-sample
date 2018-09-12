747d10a86fbb04f9de5fe03c552c515f28d471e0
fenglei <chai.fenglei@indzz.cn>
2018-09-12 19:00:36 +0800

    user-curd

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(User::class,100)->create();
+        $user = User::find(1);
+        $user->name = 'root';
+        $user->email = 'root@email.com';
+        $user->password = bcrypt('secret');
+        $user->is_admin = true;
+        $user->save();
+        
+    }
+}
